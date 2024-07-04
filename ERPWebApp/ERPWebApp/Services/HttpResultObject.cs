f204f1e6ec03f06de87a9a482369948b13740aa7
VICTOR VILORIA IBERTI <vviloriaiberti@dimaticasoftware.com>
2024-07-04 19:34:30 +0200

    Directorio de ensamblado basura eliminado.

--- ERPWebApp/ERPWebApp/Services/HttpResultObject.cs
+++
@@ -1,9 +0,0 @@
-﻿using System.Net;
-
-namespace ERPWebApp.Services{
-    public class HttpResultObject<T>{
-        public bool IsSuccess { get; set; }
-        public HttpStatusCode HttpResultCode { get; set; }
-        public T Result { get; set; }
-    }
-}
